1f2b5be9393aa913b5107bfce4aa1640d606522e
Jules Docx <jules.docx@gmail.com>
2024-09-12 20:10:48 +0200

    first gallery styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,15 +18,6 @@ export default function Card({ post }: { post: Post }) {
       ) : (
         <div className="card__cover--none" />
       )}
-      <div className="card__container">
-        <h3 className="card__title">
-          <a className="card__link" href={`/post/${post.slug.current}`}>
-            {post.title}
-          </a>
-        </h3>
-        <p className="card__excerpt">{post.excerpt}</p>
-        <p className="card__date">{formatDate(post._createdAt)}</p>
-      </div>
     </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import '~/styles/global.css'
+import '~/styles/gallery.css'
+import '~/styles/card.css'
 
 import type { AppProps } from 'next/app'
 import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
