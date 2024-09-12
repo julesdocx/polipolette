6b8e97b4093d876671f1618c48ad8adf2e3aa6a2
Jules Docx <jules.docx@gmail.com>
2024-09-12 19:35:29 +0200

    init cleanup

---
+++ agrippa.config.mjs
@@ -0,0 +1,6 @@
+// @ts-check
+import { defineConfig } from 'agrippa';
+
+export default defineConfig({
+  options: {}
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { useLiveQuery } from 'next-sanity/preview'
 
 import Card from '~/components/Card'
 import Container from '~/components/Container'
-import Welcome from '~/components/Welcome'
 import { readToken } from '~/lib/sanity.api'
 import { getClient } from '~/lib/sanity.client'
 import { getPosts, type Post, postsQuery } from '~/lib/sanity.queries'
@@ -32,11 +31,13 @@ export default function IndexPage(
   const [posts] = useLiveQuery<Post[]>(props.posts, postsQuery)
   return (
     <Container>
-      <section>
+      <section className='gallery'>
         {posts.length ? (
           posts.map((post) => <Card key={post._id} post={post} />)
         ) : (
-          <Welcome />
+          <div>
+            hello world
+          </div>
         )}
       </section>
     </Container>
